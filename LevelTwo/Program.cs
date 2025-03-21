19615de46e4a9240a2cfb1db5dd33e93cb97cfe9
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-21 00:41:58 +0000

    add code to show diferently depending on args

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -5,13 +5,31 @@ namespace LevelTwo
 {
     public class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            CanvasImage image = new CanvasImage("tux.jpg");
+            if (args.Length == 0)
+            {
+                string image_name = args[0];
 
-            image.MaxWidth(20);
 
-            AnsiConsole.Write(image);
+
+                CanvasImage tux = new CanvasImage("tux.jpg");
+
+                tux.MaxWidth(24);
+
+                AnsiConsole.Write(tux);
+            }
+            else
+            {
+                string image_width = args[1];
+                string image_name = args[0];
+                int width = int.Parse(image_width);
+                CanvasImage image = new CanvasImage(image_name);
+                image.MaxWidth(width);
+
+                AnsiConsole.Write(image);
+            }
+
         }
-
     }
+}
