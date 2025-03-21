1d3beb348330a02a879a9508710ce97fb5895e9d
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-21 17:36:34 +0000

    add faker and random seed to CarStats

--- CarStats/Program.cs
+++ CarStats/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Bogus;
+using Spectre.Console;
 
 namespace CarStats
 {
@@ -6,7 +8,21 @@ namespace CarStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+
+            Randomizer.Seed = new Random(int.Parse(args[1]));
+            string brands = new Faker().Vehicle.Manufacturer();
+            int car_number = new Faker().Random.Number(1, 20);
+
+
+            BarChart bc = new BarChart();
+            AnsiConsole.Write(new BarChart()
+         .Width(60)
+         .Label("[green bold underline]Car Sales[/]")
+         .CenterLabel()
+         .AddItem(brands, car_number, Color.Yellow)
+         .AddItem(brands, car_number, Color.Green)
+         .AddItem(brands, car_number, Color.Red));
+
         }
     }
 }
